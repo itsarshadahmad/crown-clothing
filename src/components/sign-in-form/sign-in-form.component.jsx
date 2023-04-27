176e7a3cbd7c603c998a3cf94d0d4b722fe118ba
Arshad Ahmad <arshadahmad.dev@gmail.com>
2023-04-27 21:44:15 +0530

    favicons added

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import Button from "../button/button.component";
 import { UserContext } from "../../contexts/user.context";
 
 import {
@@ -86,7 +86,7 @@ const SignInForm = () => {
 				<div className="buttons-container">
 					<Button type="submit">Sign In</Button>
 					<Button
-						buttonType={BUTTON_TYPE_CLASSES.google}
+						buttonType={"google"}
 						type="button"
 						onClick={signInWithGoogle}
 					>
