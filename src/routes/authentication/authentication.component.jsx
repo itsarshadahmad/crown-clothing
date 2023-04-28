2e35e1945b532c28f941c61e8196e04dc6debf51
Arshad Ahmad <arshadahmad.dev@gmail.com>
2023-04-28 22:53:16 +0530

    observer firebase auth, shop component and product context added

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,12 +4,12 @@ import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import "./authentication.styles.scss";
 
 const Authentication = () => {
-	return (
-		<div className="authentication-container">
-			<SignInForm />
-			<SignUpForm />
-		</div>
-	);
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
 };
 
 export default Authentication;
